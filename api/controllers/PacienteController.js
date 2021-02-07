702f7178782b41bbe6128426bd99e6c646aab86a
Ignacio <dev.ignacio.h@gmail.com>
2021-02-07 20:34:47 -0300

    insertar registro

--- api/controllers/PacienteController.js
+++ api/controllers/PacienteController.js
@@ -1,6 +1,17 @@
-// Cuando se crea un nuevo cliente
+const Paciente = require('../models/Paciente');
 
-exports.nuevoCLiente = (req, res, next) => {
-    console.log(req.body);
+// Cuando se crea un nuevo cliente
+exports.nuevoCLiente = async(req, res, next) => {
+
+  // crear datos de paciente con datos de req.body
+  const paciente = new Paciente(req.body);
+
+  try {
+    await paciente.save();
     res.json({ mensaje: 'El cliente se agregó correctamente' });
-}
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+  console.log(req.body);
+};
